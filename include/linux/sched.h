87bc9bcde38b51083531df846bf1e06cf362bfa7
Scott Wood <swood@redhat.com>
2019-10-12 01:52:13 -0500

    sched: Lazy migrate_disable processing
    Avoid overhead on the majority of migrate disable/enable sequences by
    only manipulating scheduler data (and grabbing the relevant locks) when
    the task actually schedules while migrate-disabled.  A kernel build
    showed around a 10% reduction in system time (with CONFIG_NR_CPUS=512).
    
    Instead of cpuhp_pin_lock, CPU hotplug is handled by keeping a per-CPU
    count of the number of pinned tasks (including tasks which have not
    scheduled in the migrate-disabled section); takedown_cpu() will
    wait until that reaches zero (confirmed by take_cpu_down() in stop
    machine context to deal with races) before migrating tasks off of the
    cpu.
    
    To simplify synchronization, updating cpus_mask is no longer deferred
    until migrate_enable().  This lets us not have to worry about
    migrate_enable() missing the update if it's on the fast path (didn't
    schedule during the migrate disabled section).  It also makes the code
    a bit simpler and reduces deviation from mainline.
    
    While the main motivation for this is the performance benefit, lazy
    migrate disable also eliminates the restriction on calling
    migrate_disable() while atomic but leaving the atomic region prior to
    calling migrate_enable() -- though this won't help with local_bh_disable()
    (and thus rcutorture) unless something similar is done with the recently
    added local_lock.
    
    Signed-off-by: Scott Wood <swood@redhat.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -231,6 +231,8 @@ extern void io_schedule_finish(int token);
 extern long io_schedule_timeout(long timeout);
 extern void io_schedule(void);
 
+int cpu_nr_pinned(int cpu);
+
 /**
  * struct prev_cputime - snapshot of system and user cputime
  * @utime: time spent in user mode
@@ -721,16 +723,13 @@ struct task_struct {
 	cpumask_t			cpus_mask;
 #if defined(CONFIG_SMP) && defined(CONFIG_PREEMPT_RT_BASE)
 	int				migrate_disable;
-	int				migrate_disable_update;
-	int				pinned_on_cpu;
+	bool				migrate_disable_scheduled;
 # ifdef CONFIG_SCHED_DEBUG
-	int				migrate_disable_atomic;
+	int				pinned_on_cpu;
 # endif
-
 #elif !defined(CONFIG_SMP) && defined(CONFIG_PREEMPT_RT_BASE)
 # ifdef CONFIG_SCHED_DEBUG
 	int				migrate_disable;
-	int				migrate_disable_atomic;
 # endif
 #endif
 #ifdef CONFIG_PREEMPT_RT_FULL
@@ -2144,5 +2143,7 @@ const struct sched_avg *sched_trace_rq_avg_irq(struct rq *rq);
 int sched_trace_rq_cpu(struct rq *rq);
 
 const struct cpumask *sched_trace_rd_span(struct root_domain *rd);
+
+extern struct task_struct *takedown_cpu_task;
 
 #endif
